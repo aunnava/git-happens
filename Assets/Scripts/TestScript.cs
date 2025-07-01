2748c87acb25228824ad8d57f80e36694125d1f6
Anurag Unnava <anuragunnava@gmail.com>
2025-07-01 19:18:30 +0200

    sample commit

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -5,7 +5,7 @@ public class TestScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //this is a test comment
     }
 
     // Update is called once per frame
